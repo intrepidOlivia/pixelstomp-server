4b1d3e35ce9e713abbb039dd9ebf4700e86b4824
poplopo <aethereal@gmail.com>
2018-06-09 09:35:10 -0700

    Set up functionality to query Reddit.

--- reddit-module.js
+++ reddit-module.js
@@ -1,80 +1,124 @@
 // Constants
-let APP_ID = '6cl5CbJO4GCPYg';
-let APP_SECRET = '-3a97BtpVCBAJSSM7w6motJr1eQ';
 let REDIRECT = 'http://pixelstomp.com/apps/reddit_lookup_utility/lookup.html';
+let APP_ID = require('./reddit-config').getPixelstompAppKey();
+let APP_SECRET = require('./reddit-config').getPixelstompAppSecret();
+let USER_AGENT = 'pixelstomp-reddit-querier by poplopo';
 
 // Live values
-let requestID = new Date().getSeconds();
-let bearerToken = null;
+let requestID = null;   // Shall be the timestamp, in seconds, that an access token was most recently requested.
+let expiry = null;      // Shall be the timestamp, in seconds, that the current access token will expire.
+let bearerToken = null; // Shall be the current access token.
+let queuedPaths = [];   // Queued paths to query after another bearer token is retrieved.
 
-// Makes an HTTP request
-// function makeUserRequest() {
-//     let https = require('https');
-//     let options = {
-//         method: 'GET',
-//         host: 'www.reddit.com',
-//         path: `/api/v1/authorize?client_id=${APP_ID}&response_type=${'code'}&
-//     state=${requestID}&redirect_uri=${REDIRECT}&duration=${'temporary'}&scope=${'history'}`,
-//         headers: {}
-//     };
-//
-//     let request = https.request(options, function (response) {
-//         response.setEncoding('utf8');
-//     });
-//     request.end();
-// }
+// TODO: Figure out how to page through listings.
 
-function makeAppRequest() {
+function retrieveAccessToken(callback) {
     let https = require('https');
     let options = {
         method: 'POST',
         host: 'www.reddit.com',
         path: `/api/v1/access_token`,
         headers: {
-            'Authorization': `Basic ${Buffer.from(${APP_ID}).toString('base64')}}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Authorization': `Basic ${Buffer.from(`${APP_ID}:${APP_SECRET}`).toString('base64')}`,
+            'User-Agent': USER_AGENT
         }
     }
-    let postData = `grant_type=client_credentials`;
+
+    let postQuery = `grant_type=${encodeURIComponent('client_credentials')}&
+    device_id=${encodeURIComponent(setTime())}`;
+
     let request = https.request(options, function (response) {
         if (response.statusCode == 200) {
             response.setEncoding('utf8');
-            let result = parseResponse(response);
+            parseResponse(response)
+                .then((result) => {
+                    bearerToken = result.access_token || null;
+                    setTimer(result.expires_in || new Date().getSeconds());
+                    if (callback) { callback(); }
+                    console.log('Request for bearer token complete. Bearer token has a current value of:', bearerToken);
+                })
+                .catch((errResult) => {
+                    console.log('Bearer token could not be retrieved. Result of query:', errResult);
+                });
         } else {
-            console.log('Status code received from Reddit was:', response.statusCode);
-            console.log('With the following message:', response.statusMessage);
+            throw new Error(response.statusMessage);
         }
-        // console.log('result retrieved from Reddit:', result);
-        // bearerToken = {
-        //     token: result.access_token,
-        //     expiry: result.expires_in,
-        //     scope: scope
-        // };
     });
-    request.write(postData);
+    request.write(postQuery);
+    request.on('error', function (err) {
+        throw err;
+    });
     request.end();
 }
 
-function makeAuthorizedRequest() {
+function makeAuthorizedRequest(path, callback) {
+    // First check to make sure there is an access token and it is still valid
+    if (!bearerToken) {
+        console.log('Request initiated but no bearer token was found. Requesting bearer token.');
+        queuedPaths.push(path);
+        retrieveAccessToken(makeAuthorizedRequest);
+        return;
+    }
+
+    if (expiry <= new Date().getSeconds()) {
+        console.log('Request initiated but bearer token was expired. Requesting new bearer token.');
+        queuedPaths.push(path);
+        retrieveAccessToken(makeAuthorizedRequest);
+        return;
+    }
+
+    if (!path) {
+        path = queuedPaths.shift();
+    }
+
     let https = require('https');
     let options = {
         method: 'GET',
         host: 'oauth.reddit.com',
-        path: '',
-        headers: {}
+        path: path,
+        headers: {
+            'Authorization': `bearer ${bearerToken}`,
+            'User-Agent': USER_AGENT
+        }
     };
+    let request = https.request(options, function (response) {
+        response.setEncoding('utf8');
+        parseResponse(response)
+            .then((result) => {
+                callback(result);
+            });
+    });
+    request.on('error', function (err) {
+        throw err;
+    });
+    request.end();
 }
 
+// Populates requestID with the time the access key was requested.
+function setTime() {
+    requestID = new Date().getSeconds();
+    return requestID;
+}
+
+// Populates expiry with the time the access token will expire.
+function setTimer(length) {
+    expiry = requestID + length;
+}
+
+// Parses an HTTP response in a JSON object (or returns it as a string if it cannot)
 function parseResponse(response) {
-    let result = '';
-    response.on('data', (chunk) => {
-        result += chunk;
+    return new Promise(function (resolve, reject) {
+        let result = '';
+        response.on('data', (chunk) => {
+            result += chunk;
+        });
+        response.on('end', function () {
+            try {
+                resolve(JSON.parse(result));
+            }
+            catch (e) {
+                reject(result);
+            }
+        });
     });
-    console.log('Stringified result:', result);
-    try {
-        return JSON.parse(result);
-    }
-    catch (e) {
-        return result;
-    }
 }
